3990935099acf0e0b607e38e47654b934816f466
saito400 <ken.s.530000@gmail.com>
2015-02-08 15:30:59 +0900

    add site controller

--- app/controllers/SiteController.scala
+++ app/controllers/SiteController.scala
@@ -1,14 +1,31 @@
 package controllers
 
+import models.Tables._
 import play.api.mvc.Controller
-import play.api.db.slick.DBAction
+import play.api.db.slick._
+
+//import play.api.db.slick.Config.driver.simple._  following Driver needed to be used instead of this to Delete records
+import scala.slick.driver.PostgresDriver.simple._
 
 object SiteController  extends Controller {
+
+  val Feeds = TableQuery[Feed]
 
   def index = DBAction { implicit rs =>
 
 
-    Ok(views.html.site.list())
+
+
+    val feeds = Feeds.sortBy(t => t.id).list
+
+
+
+    Ok(views.html.site.list(feeds))
+
+
+
+
+
   }
 
 
