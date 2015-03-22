4825dfb045275dac4b35da11628cc1c14cb17d4a
saito400 <ken.s.530000@gmail.com>
2015-03-22 18:32:05 +0900

    fixup! c

--- app/controllers/JsonController.scala
+++ app/controllers/JsonController.scala
@@ -83,74 +83,7 @@ object JsonController extends Controller {
     val json = Json.toJson(list)
     Ok(json)
 
-//    feedForm.bindFromRequest.fold(
-//      error => BadRequest(views.html.feed.list(Nil)),
-//      form  => {
-//        val user = FeedRow(0, form.name, form.url)
-//        Feed.insert(user)
-//        Redirect(routes.FeedController.index)
-//      }
-//    )
   }
 
-
-
-//  def index = DBAction { implicit rs =>
-//
-//    val feeds = Feeds.sortBy(t => t.id).list
-//    Ok(views.html.feed.list(feeds))
-//  }
-//
-//  case class FeedForm(id: Option[Int], name: Option[String], url: Option[String])
-//
-//  val Feeds = TableQuery[Feed]
-//
-//  val feedForm = Form(
-//    mapping(
-//      "id" -> optional(number),
-//      "name" -> optional(text),
-//      "url" -> optional(text)
-//    )(FeedForm.apply)(FeedForm.unapply)
-//  )
-//
-//  def insert = DBAction { implicit rs =>
-//
-//    feedForm.bindFromRequest.fold(
-//      error => BadRequest(views.html.feed.list(Nil)),
-//      form  => {
-//        val user = FeedRow(0, form.name, form.url)
-//        Feed.insert(user)
-//        Redirect(routes.FeedController.index)
-//      }
-//    )
-//  }
-//
-//  def edit(id: Option[Int]) = DBAction { implicit rs =>
-//    val form = if(id.isDefined) {
-//      val feed = Feeds.filter(t => t.id === id.get.bind).first
-//      feedForm.fill(FeedForm(Some(feed.id), feed.name, feed.url))
-//    } else {
-//      feedForm
-//    }
-//    Ok(views.html.feed.edit(form))
-//  }
-//
-//  def update = DBAction { implicit rs =>
-//    feedForm.bindFromRequest.fold(
-//      error => BadRequest(views.html.feed.edit(error)),
-//      form  => {
-//        val feed = FeedRow(form.id.get, form.name, form.url)
-//        Feed.filter(t => t.id === feed.id.bind).update(feed)
-//        Redirect(routes.FeedController.index)
-//      }
-//    )
-//  }
-//
-//  def delete(id: Int) = DBAction { implicit rs =>
-//    //Feed.delete(id) it does not work
-//    Feed.filter(t => t.id === id.bind).delete
-//    Redirect(routes.FeedController.index)
-//  }
-//
 
 }
